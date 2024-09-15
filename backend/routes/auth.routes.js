f3cb1b8d0926af95c05ebc46dbaeed202f86cabe
erdaserhan <erhan209@gmail.com>
2024-09-15 20:54:43 +0200

    authController updated

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,13 +1,12 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get('/signup', (req, res) => {
-    res.json({
-        date: "You hit the signup endpoint",
-    });
-});
+router.post('/signup', signup);
 
+router.post('/login', login);
 
+router.post('/logout', logout);
 
 export default router;
