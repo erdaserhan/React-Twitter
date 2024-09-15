bb73a914a806c52e15b995b44e20f8ae93af81dc
erdaserhan <erhan209@gmail.com>
2024-09-15 20:46:27 +0200

    router test ok

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
+router.get('/signup', (req, res) => {
+    res.json({
+        date: "You hit the signup endpoint",
+    });
+});
+
 
 
 export default router;
