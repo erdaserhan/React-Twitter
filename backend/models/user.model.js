7f61b694e05974d3045711b994964c7f35015bdb
erdaserhan <erhan209@gmail.com>
2024-09-16 11:24:44 +0200

    userModel updated

---
+++ backend/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: string,
+        required: true,
+        unique: true
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    followers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            default: [],
+        }
+    ]
+})
