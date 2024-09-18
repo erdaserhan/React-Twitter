3dfb00aa589b4c133b94686f3321123b5f1c7e18
erdaserhan <erhan209@gmail.com>
2024-09-18 15:31:52 +0200

    logout ok

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -90,7 +90,19 @@ export const login = async (req, res) => {
 }
 
 export const logout = async (req, res) => {
-    res.json({
-        date: "You hit the logout endpoint",
-    });
+    try{
+        res.cookie("jwt", "", {maxAge:0})
+        res.status(200).json({message: "Logged out successfully"})
+    }catch(error) {
+        console.log("eroor in logout controller", error.message);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+};
+
+export const getMe = async (req, res) => {
+    try {
+        
+    }catch(error) {
+
+    }
 }
