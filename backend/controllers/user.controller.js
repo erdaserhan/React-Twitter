fba6cf2d6b46babd53bbec8ddf7880c9d0c6dec3
erdaserhan <erhan209@gmail.com>
2024-09-19 16:58:05 +0200

    follow/unfollow tested on Postman ok

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -23,7 +23,7 @@ export const followUnfollowUser = async (req, res) => {
         const userToModify = await User.findById(id); //The user we can follow or unfollow      
         const currentUser = await User.findById(req.user._id);
         
-        if(id === req.User._id) {
+        if(id === req.user._id.toString()) {
             return res.status(404).json({ error: "You can't follow/unfollow yourself"});
         }
 
@@ -32,13 +32,16 @@ export const followUnfollowUser = async (req, res) => {
         const isFollowing = currentUser.following.includes(id); // currentUser(me) following array includes user id(const { id } = req.params;) 
         
         if(isFollowing) {
-            await User.findByIdAndUpdate(id, { $pull: {followers: req.User._id}}); //pull from the followers array. Because we unfollow them.
+            //Unfollow the user
+            await User.findByIdAndUpdate(id, { $pull: {followers: req.user._id}}); //pull from the followers array. Because we unfollow them.
             await User.findByIdAndUpdate(req.user._id, { $pull: {following: id}}); //pull from the following array of the current user.
             res.status(200).json({ error: "User unfollowed successfully"});
         }else {
-            await User.findByIdAndUpdate(id, { $push: {followers: req.User._id}}); //User that we would like to follow. we update the followers array. we push id of the currentUser
+            //follow the user
+            await User.findByIdAndUpdate(id, { $push: {followers: req.user._id}}); //User that we would like to follow. we update the followers array. we push id of the currentUser
             await User.findByIdAndUpdate(req.user._id, { $push: {following: id}}); //we add in the following array the id of the user that we followed.
-
+            //Send notification to the user
+            res.status(200).json({ error: "User followed successfully"});
         }
     } catch (error) {
         res.status(500).json({error: error.message});
