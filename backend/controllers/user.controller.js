dd46be2fd1ef391f2fc3f1494c4d4ab23684fc03
erdaserhan <erhan209@gmail.com>
2024-09-19 16:20:03 +0200

    followUnfollowUser created

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -12,4 +12,24 @@ export const getUserProfile = async (req, res) => {
         res.status(500).json({error: error.message});
         console.log("Error in getUserProfile : ", error.message);
     }
-}
+};
+
+export const followUnfollowUser = async (req, res) => {
+      
+
+    try {
+        const { id } = req.params; // It should be id because we called it id on "router.post("/follow/:id", protectRoute, followUnfollowUser)" in user.routes.js
+
+        const userToModify = await User.findById(id); //The user we can follow or unfollow      
+        const currentUser = await User.findById(req.user._id);
+        
+        if(id === req.User._id) {
+            return res.status(404).json({ message: "You can't follow/unfollow yourself"});
+        }
+        
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+        console.log("Error in followUnfollowUser : ", error.message);
+    }
+};
