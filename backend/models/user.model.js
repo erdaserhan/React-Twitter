527e15c49954bd637d51b40a79bb4118a189df70
erdaserhan <erhan209@gmail.com>
2024-09-23 22:07:19 +0200

    user.model likedPosts section added

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -50,8 +50,17 @@ const userSchema = new mongoose.Schema({
     link: {
         type: String,
         default: "",
-    }
-}, {timestamps: true});
+    },
+    likedPosts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post",
+            default: [],
+        },
+    ],
+    }, 
+    {timestamps: true}
+);
 
 const User = mongoose.model("User", userSchema);
 
