e53a6ec5272f8997c1dd78eb61c0c9bd23efacbc
erdaserhan <erhan209@gmail.com>
2024-09-24 23:00:39 +0200

    getUserPosts ok - postman ok

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -214,5 +214,25 @@ export const getFollowingPosts = async (req, res) => {
 };
 
 export const getUserPosts = async (req, res) => {
+    try {
+        const { username } = req.params;
 
+        const user = await User.findOne({username});
+        if(!user) res.status(404).json({error: "User not found"});
+
+        const posts = await Post.find( { user: user._id })
+        .sort({ createdAt: -1})
+        .populate({ 
+            path: 'user', 
+            select: "-password"})
+        .populate({ 
+            path: 'comments.user', 
+            select: "-password"
+        });
+
+        res.status(200).json(posts);
+    } catch (error) {
+        res.status(500).json({error: "Internal server error"});
+        console.log("Error in getUserPosts controller: ", error);
+    }
 }; 
