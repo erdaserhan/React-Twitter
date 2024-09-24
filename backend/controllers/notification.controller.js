5cdb36bf16197bae5e2ec9aab28cb20627648fd8
erdaserhan <erhan209@gmail.com>
2024-09-24 23:51:22 +0200

    deleteOneNotification created in extra use

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -33,3 +33,25 @@ export const deleteNotifications = async (req, res) => {
         res.status(500).json({error: "Internal server error"}); 
     }
 };
+
+/*export const deleteOneNotification = async (req, res) => {
+    try {
+        const notificationId = req.params.id;
+        const userId = req.user._id;
+        const notification = await Notification.findById(notificationId);
+
+        if(!notification) res.status(404).json({error: "Notification not found"});
+
+        if(notification.to.toString() !== userId.toString()) {
+            return res.status(403).json({ error: "You are not allowed to delete rhis notification"}) //403 status is unauthorized
+        }
+
+        await Notification.findByIdAndDelete(notificationId);
+
+        res.status(200).json({ message: "Notification deleted successfully" });
+        
+    } catch (error) {
+        console.log("Error in deleteOneNotification function: ", error.message);
+        res.status(500).json({error: "Internal server error"}); 
+    }
+};*/
