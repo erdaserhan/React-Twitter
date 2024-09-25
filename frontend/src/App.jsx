42eae55a801ff80a8659c952e19ffba3517a6ea2
erdaserhan <erhan209@gmail.com>
2024-09-25 17:57:46 +0200

    tailwind intellessence installed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
 function App() {
   return (
     <>
-      <p className='text-5xl text-red-500 bg-blue-300'>Hello from App.jsx</p>
+      <button className="btn">Button</button>
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-ghost">Ghost</button>
+<button className="btn btn-link">Link</button>
     </>
   );
 }
