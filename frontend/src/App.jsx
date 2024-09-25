31d9c2e2fc2099038de4b131abced2bb6e49b328
erdaserhan <erhan209@gmail.com>
2024-09-25 18:15:42 +0200

    First Router ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
+import { Routes } from "react-router-dom";
+
 function App() {
   return (
-    <>
-      
-    </>
+    <div className="flex max-w-6xl mx-auto">
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+      </Routes>
+    </div>
   );
 }
 
