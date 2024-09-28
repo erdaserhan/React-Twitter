bda549f927b76f13cfc0583ca6c72553811ce9c8
erdaserhan <erhan209@gmail.com>
2024-09-28 14:31:34 +0200

    suggestedUsers.length updated

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -16,8 +16,10 @@ const RightPanel = () => {
 			} catch (error) {
 				throw new Error(error.message);
 			}
-		}
-	})
+		},
+	});
+
+	if(suggestedUsers?.length === 0) return <div className="md:w-64 w-0"></div>
 
 	return (
 		<div className='hidden lg:block my-4 mx-2'>
